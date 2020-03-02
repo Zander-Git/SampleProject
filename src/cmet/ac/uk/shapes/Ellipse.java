5f7e95bd1a6891c58d349aba4626614da0a7e079
Zander <z-man88@hotmail.co.uk>
2020-03-02 14:40:56 +0000

    please work

--- src/cmet/ac/uk/shapes/Ellipse.java
+++ src/cmet/ac/uk/shapes/Ellipse.java
@@ -7,7 +7,7 @@ public class Ellipse extends EllipticalShape {
 	public Ellipse(Point centre, double majoraxis) {
 		super(centre, majoraxis);
 		// TODO Auto-generated constructor stub
-	}
+	} 
 
 	@Override
 	public void rotate() {
